4b7d692542300814fa0cc6089282251e177f85c6
Niklas <me@sodapop.se>
2019-04-24 18:10:28 +0200

    Use OTP extraction modules

--- lib/thesis/extractor.ex
+++ lib/thesis/extractor.ex
@@ -1,54 +1,66 @@
 defmodule Thesis.Extractor do
-  def extract(path, opts \\ []) do
-    case Path.extname(path) do
-      ".zip" -> extract_zip(path, opts)
-      _ -> {:error, "Archive format could not be determined"}
+  require Record
+  Record.defrecord(:file_info, Record.extract(:file_info, from_lib: "kernel/include/file.hrl"))
+  Record.defrecord(:zip_file, Record.extract(:zip_file, from_lib: "stdlib/include/zip.hrl"))
+
+  def peek_size(path) do
+    module = determine_module(path)
+
+    case module do
+      :zip -> peek_size_zip(path)
+      :erl_tar -> peek_size_tar(path)
     end
   end
 
-  # 1 KB
-  def extract_zip(filename, opts \\ []) do
-    limit = Keyword.get(opts, :limit, 1024)
-    destination = Keyword.get(opts, :destination, "/tmp")
+  def peek_size_zip(path) do
+    case :zip.table(String.to_charlist(path)) do
+      {:ok, table} ->
+        {:ok,
+         table
+         |> Enum.filter(fn row -> match?(zip_file(), row) end)
+         |> Enum.map(fn zip_file(info: file_info(size: size)) -> size end)
+         |> Enum.sum()}
 
-    case get_zip_decompression_size(filename) do
+      {:error, _} = error ->
+        error
+    end
+  end
+
+  def peek_size_tar(path) do
+    case :erl_tar.table(String.to_charlist(path), [:verbose, :compressed]) do
+      {:ok, table} ->
+        {:ok,
+         table
+         # {name, type, size, mtime, mode, uid, gid}
+         |> Enum.map(fn {_, _, size, _, _, _, _} -> size end)
+         |> Enum.sum()}
+
+      {:error, _} = error ->
+        error
+    end
+  end
+
+  def extract(path, opts \\ []) do
+    limit = Keyword.get(opts, :limit, 1024)
+
+    case peek_size(path) do
       {:ok, size} when size > limit ->
-        {:error, "Decompressed file size to large (#{size} bytes). Limit is #{limit} bytes."}
+        {:error, "Decompressed size #{size} bytes is larger than the limit #{limit} bytes."}
 
       {:ok, _} ->
-        {output, exit_code} =
-          System.cmd("unzip", ["-n", "-v", filename, "-d", destination], stderr_to_stdout: true)
+        {:ok, "Awesome"}
 
-        case exit_code do
-          0 ->
-            {:ok, output}
-
-          _ ->
-            {:error, output}
-        end
-
-      {:error, error} ->
-        {:error, error}
+      {:error, _} = error ->
+        error
     end
   end
 
-  def get_zip_decompression_size(filename) do
-    {output, exit_code} = System.cmd("unzip", ["-l", filename], stderr_to_stdout: true)
-
-    case exit_code do
-      0 ->
-        total_size_bytes =
-          output
-          |> String.split("\n", trim: true)
-          |> List.last()
-          |> String.split(~r{\s+}, trim: true)
-          |> List.first()
-          |> String.to_integer()
-
-        {:ok, total_size_bytes}
-
-      _ ->
-        {:error, output}
+  defp determine_module(path) do
+    case Path.extname(path) do
+      ".zip" -> :zip
+      ".tar" -> :erl_tar
+      ".gz" -> :erl_tar
+      _ -> :zip
     end
   end
 end
--- test/extractor_test.exs
+++ test/extractor_test.exs
@@ -3,17 +3,26 @@ defmodule Thesis.ExtractorTest do
   import Thesis.Extractor
 
   defp existing_zip_archive(), do: File.cwd!() <> "/test/test.zip"
+  defp existing_targz_archive(), do: File.cwd!() <> "/test/test.tar.gz"
   defp non_existing_zip_archive(), do: File.cwd!() <> "/test/non-existing.zip"
+  defp non_existing_targz_archive(), do: File.cwd!() <> "/test/non-existing.tar.gz"
   defp non_existing_file(), do: "/dev/null"
 
-  test "extracts zip archive" do
-    assert {:ok, output} = extract(existing_zip_archive())
-    IO.inspect(output)
-    assert {:error, _} = extract(non_existing_zip_archive())
-    assert {:error, _} = extract(non_existing_file())
+  test "fail for non existing archives" do
+    assert {:error, _} = peek_size(non_existing_zip_archive())
+    assert {:error, _} = peek_size(non_existing_targz_archive())
+    assert {:error, _} = peek_size(non_existing_file())
   end
 
-  test "handles decompressed file limit" do
+  test "peek size for .zip archive" do
+    assert {:ok, 104} = peek_size(existing_zip_archive())
+  end
+
+  test "peek size for .tar.gz archive" do
+    assert {:ok, 104} = peek_size(existing_targz_archive())
+  end
+
+  test "handles extraction size limit" do
     assert {:error, _} = extract(existing_zip_archive(), limit: 100)
     assert {:ok, _} = extract(existing_zip_archive(), limit: 200)
   end
