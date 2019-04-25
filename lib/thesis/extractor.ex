c0ca77293a795c54045282f0ec33ffde40d07a0a
Niklas <me@sodapop.se>
2019-04-25 15:54:55 +0200

    Handle extraction in separate process
    This is to mitigate decompression bomb attacks.
    The max_heap_size flag tells the garbage collector to kill the process
    if its heap size exceeds the maximum specified.

--- lib/thesis/extractor.ex
+++ lib/thesis/extractor.ex
@@ -4,15 +4,11 @@ defmodule Thesis.Extractor do
   Record.defrecord(:zip_file, Record.extract(:zip_file, from_lib: "stdlib/include/zip.hrl"))
 
   def peek_size(path) do
-    module = determine_module(path)
-
-    case module do
-      :zip -> peek_size_zip(path)
-      :erl_tar -> peek_size_tar(path)
-    end
+    archive_type = determine_archive_type(path)
+    peek_size(path, archive_type)
   end
 
-  def peek_size_zip(path) do
+  def peek_size(path, :zip) do
     case :zip.table(String.to_charlist(path)) do
       {:ok, table} ->
         {:ok,
@@ -26,8 +22,8 @@ defmodule Thesis.Extractor do
     end
   end
 
-  def peek_size_tar(path) do
-    case :erl_tar.table(String.to_charlist(path), [:verbose, :compressed]) do
+  def peek_size(path, :tar) do
+    case :erl_tar.table(to_charlist(path), [:verbose, :compressed]) do
       {:ok, table} ->
         {:ok,
          table
@@ -41,9 +37,12 @@ defmodule Thesis.Extractor do
   end
 
   def extract(path, opts \\ []) do
-    limit = Keyword.get(opts, :limit, 1024)
+    # 550 MB
+    limit = Keyword.get(opts, :limit, 576_716_800)
 
-    case peek_size(path) do
+    archive_type = determine_archive_type(path)
+
+    case peek_size(path, archive_type) do
       {:ok, size} when size > limit ->
         {:error, "Decompressed size #{size} bytes is larger than the limit #{limit} bytes."}
 
@@ -51,18 +50,63 @@ defmodule Thesis.Extractor do
         {:error, "Decompressed size is 0 bytes."}
 
       {:ok, _} ->
-        {:ok, "Awesome"}
+        pid = self()
+
+        spawn_monitor(fn ->
+          Process.flag(:max_heap_size, %{size: 2600, kill: true, error_logger: false})
+
+          send(pid, extract_archive(path, archive_type))
+        end)
+
+        receive do
+          {:DOWN, _, _, _, :normal} ->
+            receive do
+              result -> result
+            after
+              60000 -> {:error, "Timeout"}
+            end
+
+          {:DOWN, _, _, _, :killed} ->
+            {:error, "Out of memory"}
+
+          {:DOWN, _, _, _, reason} ->
+            {:error, inspect(reason)}
+        after
+          60000 -> {:error, "Timeout"}
+        end
 
       {:error, _} = error ->
         error
     end
   end
 
-  defp determine_module(path) do
+  def extract_archive(path, :zip) do
+    case :zip.extract(to_charlist(path), [:memory]) do
+      {:ok, files} ->
+        {:ok, Enum.map(files, &filename_to_string/1)}
+
+      {:error, _} = error ->
+        error
+    end
+  end
+
+  def extract_archive(path, :tar) do
+    case :erl_tar.extract(String.to_charlist(path), [:compressed, :memory]) do
+      {:ok, files} ->
+        {:ok, Enum.map(files, &filename_to_string/1)}
+
+      {:error, _} = error ->
+        error
+    end
+  end
+
+  defp filename_to_string({filename, data}), do: {to_string(filename), data}
+
+  defp determine_archive_type(path) do
     case Path.extname(path) do
       ".zip" -> :zip
-      ".tar" -> :erl_tar
-      ".gz" -> :erl_tar
+      ".tar" -> :tar
+      ".gz" -> :tar
       _ -> :zip
     end
   end
--- test/extractor_test.exs
+++ test/extractor_test.exs
@@ -4,12 +4,16 @@ defmodule Thesis.ExtractorTest do
 
   defp existing_zip_archive(), do: File.cwd!() <> "/test/test.zip"
   defp existing_targz_archive(), do: File.cwd!() <> "/test/test.tar.gz"
+  defp existing_nested_zip_archive(), do: File.cwd!() <> "/test/test_nested.zip"
+  defp existing_nested_targz_archive(), do: File.cwd!() <> "/test/test_nested.tar.gz"
   defp non_existing_zip_archive(), do: File.cwd!() <> "/test/non-existing.zip"
   defp non_existing_targz_archive(), do: File.cwd!() <> "/test/non-existing.tar.gz"
   defp non_existing_file(), do: "/dev/null"
 
   defp evil_header_zip_archive(), do: File.cwd!() <> "/test/10GB.zip"
   defp evil_header_targz_archive(), do: File.cwd!() <> "/test/10GB.tar.gz"
+
+  defp zip_bomb_archive(), do: File.cwd!() <> "/test/10GB_2.zip"
 
   test "fail for non existing archives" do
     assert {:error, _} = peek_size(non_existing_zip_archive())
@@ -34,4 +38,35 @@ defmodule Thesis.ExtractorTest do
     assert {:error, _} = extract(existing_zip_archive(), limit: 100)
     assert {:ok, _} = extract(existing_zip_archive(), limit: 200)
   end
+
+  test "correct extraction of files" do
+    assert {:ok, [{"HelloWorld.java", data}]} = extract(existing_zip_archive(), limit: 200)
+    assert String.length(data) > 0
+    assert {:ok, [{"HelloWorld.java", data}]} = extract(existing_targz_archive(), limit: 200)
+    assert String.length(data) > 0
+  end
+
+  test "correct extraction of nested files" do
+    assert {:ok, [{"HelloWorld.java", data}, {"nested/HelloWorld_nested.java", data2}]} =
+             extract(existing_nested_zip_archive(), limit: 300)
+
+    assert String.length(data) > 0
+    assert String.length(data2) > 0
+
+    assert {:ok, [{"HelloWorld.java", data}, {"nested/HelloWorld_nested.java", data2}]} =
+             extract(existing_nested_targz_archive(), limit: 300)
+
+    assert String.length(data) > 0
+    assert String.length(data2) > 0
+  end
+
+  test "handles zip bomb" do
+    assert {:error, _} = extract(zip_bomb_archive(), limit: 1_895_825_409)
+  end
+
+  # NOTE: This works, but I don't want to commit a 550 MB file in git
+  # defp large_zip_archive(), do: File.cwd!() <> "/test/random.dat.zip"
+  # test "handles large archives" do
+  #   assert {:ok, _} = extract(large_zip_archive())
+  # end
 end
