3c7635630ed899b29d59af38b9568bf06a50761c
Johan Magnusson <jcmmagnusson@gmail.com>
2019-05-06 20:58:10 +0200

    Adds some more functions for Elixir environment

--- lib/thesis/dsl/elixir.ex
+++ lib/thesis/dsl/elixir.ex
@@ -10,4 +10,18 @@ defmodule Thesis.DSL.Elixir do
   def help() do
     "mix help"
   end
+
+  def create_project(name) do
+    """
+    mix new #{name}
+    rm #{name}/lib/*.ex #{name}/test/*_test.ex
+    """
+  end
+
+  def test(project) do
+    """
+    cd #{project}
+    mix test
+    """
+  end
 end
