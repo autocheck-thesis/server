962d73a1009bd960ac50062ecbf9a0344e0ba667
Niklas <me@sodapop.se>
2019-05-07 14:05:50 +0200

    Submission sidebar template

--- lib/thesis_web/views/submission_view.ex
+++ lib/thesis_web/views/submission_view.ex
@@ -1,6 +1,25 @@
 defmodule ThesisWeb.SubmissionView do
   use ThesisWeb, :view
 
+  def main_sidebar_items(conn, assignment),
+    do: [
+      %{title: "Submit", href: Routes.submission_path(conn, :index, assignment.id)},
+      %{
+        title: "Previous submissions",
+        href: Routes.submission_path(conn, :previous, assignment.id)
+      }
+    ]
+
+  def submission_sidebar_items(conn, submission),
+    do: [
+      %{title: "Build log", href: Routes.submission_path(conn, :show, submission.id)},
+      %{title: "Files", href: Routes.submission_path(conn, :files, submission.id)},
+      %{
+        title: "Back to assignment",
+        href: Routes.submission_path(conn, :submit, submission.assignment)
+      }
+    ]
+
   def file_header_icon(:added), do: "plus square outline"
   def file_header_icon(:changed), do: "pencil alternate"
   def file_header_icon(:unchanged), do: "file outline"
