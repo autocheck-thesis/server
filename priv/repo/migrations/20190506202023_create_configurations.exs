3bc65971ef6871e5d00c39d2207e728786a8d489
Niklas <me@sodapop.se>
2019-05-21 13:35:43 +0200

    Change code type from VARCHAR(255) to TEXT

--- priv/repo/migrations/20190506202023_create_configurations.exs
+++ priv/repo/migrations/20190506202023_create_configurations.exs
@@ -4,7 +4,7 @@ defmodule Thesis.Repo.Migrations.CreateConfigurations do
   def change do
     create table(:configurations, primary_key: false) do
       add(:id, :uuid, primary_key: true)
-      add(:code, :string)
+      add(:code, :text)
       add(:assignment_id, references(:assignments, type: :binary_id, on_delete: :delete_all))
 
       timestamps()
